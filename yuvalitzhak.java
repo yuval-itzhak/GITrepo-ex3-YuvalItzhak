2c7768cf53f3c0ca5ddd16adb7079c3dd67a03e4
yuval-itzhak <yuval.itzhak3@gmail.com>
2025-03-29 19:59:23 +0300

    change in yuvalitzhak.java file

--- yuvalitzhak.java
+++ yuvalitzhak.java
@@ -1,7 +1,7 @@
 public class yuvalitzhak{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Yuval Itzhak..."+i);
+			System.out.println("My name is Yuval..."+i);
 		}
 	}
 }
